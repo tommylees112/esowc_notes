585493b1355d1dfa6afcb46ea79d38e3ea02aacb
tommylees112 <thomas.lees112@gmail.com>
2019-05-12 23:00:30 +0100

    TEST USING ONLY FIRST 5 values

--- src/clean_vhi_parallel.py
+++ src/clean_vhi_parallel.py
@@ -7,7 +7,7 @@ import pathlib
 if os.getcwd().split('/')[-2] == "eswoc_notes":
     sys.path.append('..')
 print(os.getcwd())
-from src.clean_vhi_data import preprocess_VHI_data
+from clean_vhi_data import preprocess_VHI_data
 
 IN_FILE_DIR="/soge-home/projects/crop_yield/ESoWC_dummy/data/vhi/ftp.star.nesdis.noaa.gov/pub/corp/scsb/wguo/data/Blended_VH_4km/VH/"
 # OUT_FILE_DIR="/soge-home/projects/crop_yield/ESoWC_dummy/data/vhi/clean2"
