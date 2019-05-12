d9269b32021a294063d66c6f5f1ee0a2737ea325
tommylees112 <thomas.lees112@gmail.com>
2019-05-12 22:25:33 +0100

    vhi addthe kenya subsetting functionality

--- src/clean_vhi_parallel.py
+++ src/clean_vhi_parallel.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import multiprocessing
-from clean_vhi_data import preprocess_VHI_data
+from src.clean_vhi_data import preprocess_VHI_data
 
 IN_FILE_DIR="/soge-home/projects/crop_yield/ESoWC_dummy/data/vhi/ftp.star.nesdis.noaa.gov/pub/corp/scsb/wguo/data/Blended_VH_4km/VH/"
 # OUT_FILE_DIR="/soge-home/projects/crop_yield/ESoWC_dummy/data/vhi/clean2"
@@ -25,7 +25,7 @@ if __name__ == "__main__":
   print(f"Writing data to: {OUT_FILE_DIR}")
   nc_files = glob.glob(IN_FILE_DIR+"*VH.nc")[:5]
   os.system(f"mkdir -p {OUT_FILE_DIR}")
-  assert os.path.isdir(OUT_FILE_DIR), f"The output file {out_file_dir} does not exist!"
+  assert os.path.isdir(OUT_FILE_DIR), f"The output file {OUT_FILE_DIR} does not exist!"
 
   pool = multiprocessing.Pool(processes=100)
   pool.map(add_coordinates, nc_files)
