a08818bd64fb4a715d72db803cf3fd812bd5044b
tommylees112 <thomas.lees112@gmail.com>
2019-05-12 21:32:12 +0100

    vhi test

---
+++ example_scripts/vhi_test.py
@@ -0,0 +1,10 @@
+from src.api_helpers import Region
+import src.clean_vhi_data
+from pathlib import Path
+import xarray as xr
+import numpy as np
+
+data_dir = Path("/soge-home/projects/crop_yield/ESoWC_dummy/data/vhi/ftp.star.nesdis.noaa.gov/pub/corp/scsb/wguo/data/Blended_VH_4km/VH")
+netcdf_filepaths = [f for f in data_dir.glob('*')]
+
+netcdf_filepath = netcdf_filepaths[0]
