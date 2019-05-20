21e672e49982728c96a67a09172115adeab3622e
tommylees112 <thomas.lees112@gmail.com>
2019-05-20 16:04:43 +0100

    testing different download scripts for different datasets

---
+++ example_scripts/download_chirps.py
@@ -0,0 +1,62 @@
+"""
+ftp://ftp.chg.ucsb.edu
+
+ftp://ftp.chg.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/global_pentad/netcdf/
+"""
+
+from typing import List, Optional
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+
+data_dir = Path('data/')
+
+def get_chirps_filenames() -> List:
+    """
+    ftp://ftp.chg.ucsb.edu/pub/org/chg/products/
+        CHIRPS-2.0/global_pentad/netcdf/
+    """
+    base_url = 'ftp://ftp.chg.ucsb.edu'
+    url = base_url + '/pub/org/chg/products/CHIRPS-2.0/global_pentad/netcdf/'
+
+    # use urllib.request to read the page source
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    the_page = response.read()
+
+    # use BeautifulSoup to parse the html source
+    page = str(BeautifulSoup(the_page))
+
+    # split the page to get the filenames as a list
+    firstsplit=page.split('\r\n')  # split the newlines
+    secondsplit = [x.split(' ') for x in firstsplit]  # split the spaces
+    flatlist = [item for sublist in secondsplit for item in sublist]  # flatten
+    chirpsfiles = [x for x in flatlist if 'chirps' in x]
+
+    return chirpsfiles
+
+
+def wget_file(filepath: str, data_dir: Path):
+    os.system(f"wget -np -nH --cut-dirs 7 {filepath} -P {data_dir.as_posix()}")
+
+
+def download_chirps_files(chirps_files, data_dir: Path):
+    """ download the chirps files using wget """
+    # build the base url
+    base_url = 'ftp://ftp.chg.ucsb.edu/pub/org/chg'
+    base_url += '/products/CHIRPS-2.0/global_pentad/netcdf/'
+    if base_url[-1] != '/':
+        base_url += '/'
+
+    filepaths = [base_url + f for f in chirps_files]
+
+    pool = multiprocessing.pool(processes=100)
+    pool.map(wget_file, filepaths, data_dir)
+
+
+chirps_out_dir = data_dir / "raw" / "chirps"
+if not chirps_out_dir.exists():
+    chirps_out_dir.mkdir()
+
+chirps_files = get_chirps_filenames()
+download_chirps_files(chirps_files, chirps_out_dir)
---
+++ example_scripts/ftps_list_dir.py
@@ -0,0 +1,44 @@
+# https://gist.github.com/Ryanb58/43e8bf5a8935405c455c5b41f8f8a0a3
+# WOrks in python 2.7 not sure if it works in python 3.
+
+# Just straight up connect by any means possible.
+from ftplib import FTP_TLS
+
+def connect():
+    ftp = FTP_TLS()
+    ftp.debugging = 2
+    ftp.connect('localhost', 2121)
+    ftp.login('developer', 'password')
+    return ftp
+
+ftp = connect()
+ftp.retrlines('LIST')
+
+
+# Connect, but only using SSL version 2 aor 3
+from ftplib import FTP_TLS
+import ssl
+
+def connect():
+    ftp = FTP_TLS()
+    ftp.ssl_version = ssl.PROTOCOL_SSLv23
+    ftp.debugging = 2
+    ftp.connect('localhost', 2121)
+    ftp.login('developer', 'password')
+    return ftp
+
+
+# Connect, but only using TLS version 1.2
+from ftplib import FTP_TLS
+import ssl
+
+def connect():
+    ftp = FTP_TLS()
+    ftp.ssl_version = ssl.PROTOCOL_TLSv1_2
+    ftp.debugging = 2
+    ftp.connect('localhost', 2121)
+    ftp.login('developer', 'password')
+    return ftp
+
+ftp = connect()
+ftp.retrlines('LIST')
