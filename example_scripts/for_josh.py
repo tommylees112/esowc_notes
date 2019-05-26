2acbf86273edf2c6beb7b249752e3de91e0b09fd
tommylees112 <thomas.lees112@gmail.com>
2019-05-26 20:47:16 +0100

    script for josh

---
+++ example_scripts/for_josh.py
@@ -0,0 +1,45 @@
+"""
+Testing the ERA5ExporterPOS class.
+
+See the notebooks here:
+https://github.com/planet-os/notebooks/blob/master/aws/era5-pds.md
+https://github.com/planet-os/notebooks/blob/master/aws/era5-s3-via-boto.ipynb
+
+Run the lines starting with "$" using bash
+
+1. Download the repository
+
+    $ git clone https://github.com/esowc/ml_drought.git
+
+2. change to the repository directory
+
+    $ cd ml_drought
+
+3. create the environment with the necessary packages
+
+    $ conda env create -f environment.ubuntu.cpu.yml
+
+4. activate the python environment with the packages installed
+
+    $ conda activate esowc-drought
+
+5. then run the lines below (perhaps copying them into
+    an ipython console or however you run your code)
+
+"""
+
+from pathlib import Path
+
+assert Path('.').absolute().name == "ml_drought", f"You have to run this script from the ml_drought repository"
+
+
+from src.exporters import ERA5ExporterPOS
+
+# initialise the ERA5 exporter object
+e = ERA5ExporterPOS()
+
+# download precip for 2010 - 2018 (the download should start)
+e.export('precipitation_amount_1hour_Accumulation', years=[y for y in range(2010,2019)])
+
+####
+# your code analysing the downloaded data 
--- example_scripts/test_planetOS.py
+++ example_scripts/test_planetOS.py
@@ -26,7 +26,8 @@ from src.exporters import ERA5ExporterPOS
 
 e = ERA5ExporterPOS()
 
-e.export('precipitation_amount_1hour_Accumulation')
+# download precip for 2010 - 2018
+e.export('precipitation_amount_1hour_Accumulation', years=[y for y in range(2010,2019)])
 
 variables = ['air_pressure_at_mean_sea_level', 'air_temperature_at_2_metres', 'air_temperature_at_2_metres_1hour_Maximum', 'air_temperature_at_2_metres_1hour_Minimum', 'dew_point_temperature_at_2_metres', 'eastward_wind_at_100_metres', 'eastward_wind_at_10_metres', 'integral_wrt_time_of_surface_direct_downwelling_shortwave_flux_in_air_1hour_Accumulation', 'lwe_thickness_of_surface_snow_amount', 'northward_wind_at_100_metres', 'northward_wind_at_10_metres', 'precipitation_amount_1hour_Accumulation', 'sea_surface_temperature', 'snow_density', 'surface_air_pressure']
 
